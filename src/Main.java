6ba44746d00f72d4cdadfe4731112e921800b695
Alex Gilbert <alexgilbert@Alexs-MacBook-Air-6.local>
2024-09-30 18:20:59 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+//Group E Project
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String yesOrNo;
+        System.out.println("Do you want to open your recipe manager: ");
+        yesOrNo = scan.nextLine();
+        while(yesOrNo.equals("Yes")){
+
+        }
+    }
+}
